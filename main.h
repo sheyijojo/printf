ac6758aaaa6bfd78648934aa02e5d208451c4da1
Michael <micfem2b@gmail.com>
2022-08-09 16:54:55 -0700

    main header file

--- main.h
+++ main.h
@@ -2,6 +2,22 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define FIELD_BUF_SIZE 50
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
 
 int _putchar(char c);
 int print_int(va_list arg);
