dd58f0841ef0ce1073e9ea4ac25409fb1d11bb94
Sheyi Gaji <seezie@gmail.com>
2022-08-09 22:45:54 -0700

    Remodeled _printf

--- _printf.c
+++ _printf.c
@@ -1,41 +1,45 @@
-       return (0);
-}
+#include "main.h"
 /**
- * _printf - mimic printf from stdio
- * Description: produces output according to a format
- * write output to stdout, the standard output stream
- * @format: character string composed of zero or more directives
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
- * return -1 for incomplete identifier error
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
-        int sum = 0;
-        va_list ap;
-        char *p, *start;
-        params_t params = PARAMS_INIT;
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
 
-        va_start(ap, format);
+	va_list args;
+	int i = 0, j, len = 0;
 
-        if (!format || (format[0] == '%' && !format[1]))
-                return (-1);
-        if (format[0] == '%' && format[1] == ' ' && !format[2])
-                return (-1);
-        for (p = (char *)format; *p; p++)
-        {
-                init_params(&params, ap);
-                if (*p != '%')
-                {
-                        sum += _putchar(*p);
-                        continue;
-                }
-                start = p;
-                p++;
-                while (get_flag(p, &params)) /* while char at p is flag char */
-                {
-                        p++; /* next char */
-                }
-                p = get_width(p, &
-~
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
+}
